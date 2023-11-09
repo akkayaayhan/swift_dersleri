1cf8fa2f0523efc7d6946ca1c1de9cd3e1bb28fa
ayhanakkaya <ayhanakkayameb@gmail.com>
2023-11-08 16:24:57 -0800

    Sanat kitabı projesi yapılıyor

---
+++ SanatKitabiProjesi/SanatKitabiProjesi/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  SanatKitabiProjesi
+//
+//  Created by ayhanakkaya on 7.11.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ demo2/demo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo2
+//
+//  Created by ayhanakkaya on 4.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
